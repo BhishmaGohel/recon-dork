35a115c0131267339bed22dbac357a55a6937064
Bhishma Gohel <52067605+BhishmaGohel@users.noreply.github.com>
2026-06-16 16:42:57 +0530

    Revert "feat: Updated Dorklist (#32)"
    This reverts commit 18c1dc9e6918e68c84d155502f834c1371a5ef36.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -39,11 +39,6 @@ export function generateSearchUrl(
 ): string {
   const finalDork = template.replace(/{query}/g, encodeURIComponent(query))
 
-  // If the template is already a full URL, return it directly
-  if (template.includes('https://') || template.includes('http://')) {
-    return finalDork
-  }
-
   const engines: Record<string, string> = {
     google: 'https://www.google.com/search?q=',
     clouds: 'https://www.google.com/search?q=',
